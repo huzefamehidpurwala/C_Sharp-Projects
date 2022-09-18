5b3c810164a72cbb91c43c179d7c8017992062a7
Huzefa Mehidpurwala <94836331+huzefamehidpurwala@users.noreply.github.com>
2022-09-18 10:13:24 +0530

    First-Commit

---
+++ PropertyExample/PropertyExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PropertyExample
+{
+    class Person
+    {
+        private string name = "Huzefa"; // field
+
+        public string Name   // property
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Liam";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
